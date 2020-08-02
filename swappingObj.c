f6309209f71c03468dcd79a334982562f1f69191
nevzat <nevzatseferoglu@gmail.com>
2020-08-02 03:13:59 +0300

    XOR-2

--- swappingObj.c
+++ swappingObj.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 
+void swap( size_t* v1 , size_t* v2 ) {
+
+    /* Caution: do not forget to check whether adresses are pointing to the same location. */
+    /* If you do not do that , it will replace both variable with zero. */
+
+    if( v1 == v2 ) {
+        return;
+    }
+
+    *v1 = *v1 ^ *v2;
+    *v2 = *v1 ^ *v2;
+    *v1 = *v1 ^ *v2;
+
+}
+
 void main( void ) {
 
+    
     /* Swapping two variable with XORing */
+
     size_t v1;
     size_t v2;
-    size_t tempXOR;
 
     v1 = 3;
     v2 = 4;
 
     printf("v1: %d\n",v1);
     printf("v2: %d\n",v2);
-
-    /* Note that , v1 behaves like an temprorary XOR variable .*/
     
-    v1 = v1 ^ v2;
-    v2 = v1 ^ v2;
-    v1 = v1 ^ v2;
+    swap(&v1,&v2);
 
     printf("v1: %d\n",v1);
     printf("v2: %d\n",v2);
