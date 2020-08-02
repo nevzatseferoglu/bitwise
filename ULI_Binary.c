9fc78ce7143f2514e638152c863dca7c9bc5ec4d
nevzat <nevzatseferoglu@gmail.com>
2020-08-02 22:19:14 +0300

    unsigned long int binary output

---
+++ ULI_Binary.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void UNSIGNED_LONG_INT_BINARY( const unsigned long int x ) {
+
+    /* masking */
+    unsigned long int i = ~(~((unsigned long int )0) >> 1);
+
+    /* traversing bit by bit */
+    while (i) {
+        putchar (x & i ? '1' : '0');
+        i >>= 1;
+    }
+    putchar('\n');
+}
+
+void main( void ) {
+    UNSIGNED_LONG_INT_BINARY(17);  
+}
